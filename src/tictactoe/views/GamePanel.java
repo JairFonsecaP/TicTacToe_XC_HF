ab2a11a5e86bc5ee9d6f116d0359e61edf3caa3c
Ximena <ximenacarrilloh@gmail.com>
2022-01-21 13:36:18 -0500

    merge

--- src/tictactoe/views/GamePanel.java
+++ src/tictactoe/views/GamePanel.java
@@ -8,17 +8,15 @@ import java.awt.*;
 import java.util.ArrayList;
 
 public class GamePanel extends JPanel {
-    private final ArrayList<SingleSquareGame> fields;
-    private final int size;
     private final Player player1;
     private final Player player2;
 
-public class GamePanel extends JPanel
-{
     private final GameZonePanel gameZonePanel;
     private final InformationPanel informationPanel;
     public GamePanel(int sizeSide, Player player1, Player player2 )
     {
+        this.player1 = player1;
+        this.player2 = player2;
         gameZonePanel = new GameZonePanel(sizeSide);
         informationPanel = new InformationPanel();
         setLayout(new BoxLayout(this,BoxLayout.Y_AXIS));
